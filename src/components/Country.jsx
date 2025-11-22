e4c9008bf84df885c3b977edf2aa1a2e7512d5a5
ink <bjaghet2014@gmail.com>
2025-11-22 14:01:45 +0100

    complete 2.20, add weather section that returns temp weather icon and temp

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,5 +1,25 @@
+import {useState, useEffect} from 'react'
+
 const Country = ({ country }) => {
     const { name, capital, area, languages, flags } = country
+
+    const [weather,setWeather] = useState(null)
+    const api_key = import.meta.env.VITE_WEATHER_KEY
+
+    useEffect(()=>{
+        if (!capital || capital.length === 0) return
+        const city = capital[0]
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            setWeather(data)
+        })
+        .catch(err => console.error("Weather error:", err))
+    }, [capital, api_key])
+
+
 
     return (
         <div>
@@ -15,7 +35,21 @@ const Country = ({ country }) => {
         </ul>
 
         <img src={flags?.png || ""} alt={flags?.alt || name.common} width="150" />
+        {weather && weather.main ? (
+            <div>
+                <h2>Weather in {capital[0]}</h2>
+                <p>Temperature: {weather.main.temp} °C</p>
+                <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} 
+                alt="weather icon" />
+                <p>Wind: {weather.wind.speed} m/s</p>
+            </div>
+            ): (
+                <p>Loading weather...</p>
+            )
+            }
         </div>
+
+        
     )
 }
 
